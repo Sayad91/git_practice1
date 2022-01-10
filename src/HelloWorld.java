2c8906bb392f46e83b6ea47d6b9486866185d75c
sayad <sayadeligne@gmail.com>
2022-01-10 11:49:03 -0600

    work 2 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
 
-    //Ding some work here (you can type anything you want)
+    //Doing some work here (you can type anything you want)
 
-    //doing awesome work 2 !
+    //Doing awesome work 2 !
     //
 
 }
